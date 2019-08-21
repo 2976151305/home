c35ee49ad66e135f29e8b8b949fe4a11028cfdda
unknown <2976151305@qq.com>
2019-08-21 18:23:59 +0800

    .

--- js/mian.js
+++ js/mian.js
@@ -19,91 +19,22 @@ var active = {
     })
   },
   /**
-   * 表单弹框
-   * @param {object} config layer弹出层设置
-   * @param {string} api_url api路径
-   * @param {object} modules 需要重新加载的layui模块
+   * layui tab-content切换效果
+   * @param {obj} el dom元素
    */
-  openForm: function (config, api_url, modules = {}) {
-    layer.open({
-      title: config.title || '请填写信息',
-      content: config.content,
-      shade: config.shade || .5,
-      closeBtn: false,
-      shadeClose: true,
-      btn: config.btn || '提交',
-      area: config.area,
-      move: false,
-      success: function () {
-        if ($("#distpicker")[0]) $("#distpicker").distpicker()
-        // 重新加载模块
-        if (modules.form) modules.form.render()
-        if (modules.upload) modules.upload.render()
-      },
-      yes: (index, layero) => {
-        return new Promise((resolve, reject) => {
-          let dataObj = []
-          let id_arr = $('.layui-input-block *')
-          id_arr.each((i, item) => {
-            // 检索所有带有id的表单元素
-            if (item.id) {
-              if (item.tagName === 'INPUT') {
-                let val = item.value
-                if (!val.length) {
-                  reject(false)
-                  return false
-                }
-                dataObj.push({
-                  [item.id]: val
-                })
-              }
-            }
-          })
-          resolve(dataObj)
-        }).then(r => {
-          console.log(r)
-          active.query(api_url, dataObj, 'POST')
-        }).then(c => {
-          console.log(c)
-          active.successCallBack('提交成功', index)
-        }).catch(err => {
-          if (!err) {
-            active.failCallBack('请输入完整的信息', err)
-          }
-        })
-      }
-    })
-  },
-  /**
-   * 成功回调
-   * @param {string} msg 提示信息
-   * @param {number} index 弹窗索引
-   */
-  successCallBack: function (msg, index) {
-    layer.alert(msg, {
-      icon: 1
-    })
-    layer.close(index)
-  },
-  /**
-   * 失败回调
-   * @param {string} msg 提示信息
-   * @param {obj} err 错误提示
-   */
-  failCallBack: function (msg, err) {
-    layer.msg(msg)
-    new Error(err)
-  },
   tabChangeAnimate: function (el) {
     let $tabItem = el.parent().siblings('.layui-tab-content').children('.layui-tab-item')
     $tabItem.removeClass('layui-anim layui-anim-fadein')
     $tabItem.eq(el.index()).addClass('layui-anim layui-anim-fadein')
+  },
+  /**
+   * 获取参数值
+   * @param {string} name 参数名
+   */
+  getQueryString: function (name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return decodeURI(r[2]);
+    return null;
   }
-}
-
-function getQueryString(name) {
-  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-  var r = window.location.search.substr(1).match(reg);
-  if (r != null) return decodeURI(r[2]);
-  return null;
 }
